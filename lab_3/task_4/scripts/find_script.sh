203288f5f1000f2ff65cc74cb040a2d0ec0b727d
Denys Kudriev <denys_kudriev@epam.com>
2023-01-14 18:59:43 +0200

    Move data to scripts folder, add pdf

--- lab_3/task_4/scripts/find_script.sh
+++ lab_3/task_4/scripts/find_script.sh
@@ -1,8 +1,8 @@
 echo "Importing db"
-mongoimport --host mongo_main_server:27017 --db db --collection london_postcodes --type=csv --headerline --file='data/London postcodes.csv'
-mongoimport --host mongo_main_server:27017 --db db --collection clients --type=json --file=data/clients.json
-mongoimport --host mongo_main_server:27017 --db db --collection drivers --type=json --file=data/drivers.json
-mongoimport --host mongo_main_server:27017 --db db --collection orders --type=json --file=data/orders.json
+mongoimport --host mongo_main_server:27017 --db db --collection london_postcodes --type=csv --headerline --file=scripts/london_postcodes.csv
+mongoimport --host mongo_main_server:27017 --db db --collection clients --type=json --file=scripts/clients.json
+mongoimport --host mongo_main_server:27017 --db db --collection drivers --type=json --file=scripts/drivers.json
+mongoimport --host mongo_main_server:27017 --db db --collection orders --type=json --file=scripts/orders.json
 echo "Running script in db"
 mongosh --host mongo_main_server:27017 < scripts/mongo_script.js
 echo "Script execution finished"
